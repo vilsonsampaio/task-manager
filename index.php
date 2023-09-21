20bb7df5f2b30bc3b49d7dc7a6ab8c036be8a0ab
mfarnour <mfarnour@gmail.com>
2023-09-21 18:19:09 +0330

    feat: adding changes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Task Manager</title>
+  <title>Study Manager</title>
 
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
